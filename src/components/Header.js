21311f567f8767386d08ea736fe62ca631247f69
Tareq Rana <rafat.webdeveloper@gmail.com>
2022-09-13 13:06:07 +0600

    Complete Add todo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,50 @@
+import { useState } from "react";
 import tickImage from "../assets/images/double-tick.png";
 import noteImage from "../assets/images/notes.png";
 import plusImage from "../assets/images/plus.png";
+import { useAddTodoMutation } from "../features/api/apiSlice";
 
 export default function Header() {
-    return (
-        <div>
-            <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
-                <img src={noteImage} className="w-6 h-6" alt="Add todo" />
-                <input
-                    type="text"
-                    placeholder="Type your todo"
-                    className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
-                />
-                <button
-                    type="submit"
-                    className={`appearance-none w-8 h-8 bg-[url('${plusImage}')] bg-no-repeat bg-contain`}
-                ></button>
-            </form>
+  const [addTodo, { data: todo, isLoading, isError }] = useAddTodoMutation();
+  const [inputText, setInputText] = useState("");
 
-            <ul className="flex justify-between my-4 text-xs text-gray-500">
-                <li className="flex space-x-1 cursor-pointer">
-                    <img className="w-4 h-4" src={tickImage} alt="Complete" />
-                    <span>Complete All Tasks</span>
-                </li>
-                <li className="cursor-pointer">Clear completed</li>
-            </ul>
-        </div>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo({
+      text: inputText,
+      completed: false,
+      color: "",
+    });
+    setInputText("");
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+      >
+        <img src={noteImage} className="w-6 h-6" alt="Add todo" />
+        <input
+          type="text"
+          value={inputText}
+          placeholder="Type your todo"
+          onChange={(e) => setInputText(e.target.value)}
+          className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+        />
+        <button
+          type="submit"
+          className={`appearance-none w-8 h-8 bg-[url('${plusImage}')] bg-no-repeat bg-contain`}
+        ></button>
+      </form>
+
+      <ul className="flex justify-between my-4 text-xs text-gray-500">
+        <li className="flex space-x-1 cursor-pointer">
+          <img className="w-4 h-4" src={tickImage} alt="Complete" />
+          <span>Complete All Tasks</span>
+        </li>
+        <li className="cursor-pointer">Clear completed</li>
+      </ul>
+    </div>
+  );
 }
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,11 +5,22 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://learn-with-summit-server.herokuapp.com",
   }),
+  tagTypes: ["todos"],
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "/todos",
+      keepUnusedDataFor: 600,
+      providesTags: ["todos"],
+    }),
+    addTodo: builder.mutation({
+      query: (data) => ({
+        url: "/todos",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["todos"],
     }),
   }),
 });
 
-export const { useGetTodosQuery } = apiSlice;
+export const { useGetTodosQuery, useAddTodoMutation } = apiSlice;
